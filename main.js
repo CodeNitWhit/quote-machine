2ff381381983ccd0f238304ce5dab32e848744bb
Codie Whitaker <CWhitaker2@student.fullsail.edu>
2021-01-07 20:22:21 -0500

    Almost Finished
    Everything is done except the share icons and a title

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+let quotesData;
+var currentQuote = '',
+  currentAuthor = '';
+
 function setNewColor() {
     var colors = [
         "#16a085",
@@ -15,39 +19,91 @@ function setNewColor() {
     ];
     var index = Math.floor(Math.random() * colors.length);
     var color = colors[index];
-    var backgroundStr = "radial-gradient(circle, rgba(153,152,152,1) 5%, rgba(239,211,206,1) 55%, " + color + " 90%)";
-    $("body").css( "background", backgroundStr);
+    var bodyStr = "radial-gradient(circle, rgba(153,152,152,1) 5%, rgba(239,211,206,1) 55%, " + color + " 90%)";
+    var quoteBoxStr = "6px 6px " + color;
+    var quoteAuthShadow = "1px 1px " + color;
+    var quoteMarkShadow = "2px 2px " + color;
+    $("body").css( "background", bodyStr);
     $('#quoteMark').css("color", color);
     $('#randoNext').css("color", color);
+    $('.showQuote').css("box-shadow", quoteBoxStr);
+    $('#quote-text').css("color", color);
+    $('#quote-author').css("text-shadow", quoteAuthShadow);
+    $('#open-quote-mark').css("text-shadow", quoteMarkShadow);
 }
 function animateLips() {
-    var mouthHeight = $("#mouth").css("width");
-    var mouthWidth = $("#mouth").css("width");
-    var lipsHeight = $("#lips").css("width");
-    var lipsWidth = $("#lips").css("width");
-    //alert("Lips: " + lipsWidth + " x " + lipsHeight + "\r\nMouth: " + mouthWidth + " x " + mouthHeight);
     $(".hideQuote").stop();
     $(".hideQuote").fadeOut(100);
     $( "#lips" ).toggle({ effect: "scale", direction: "vertical"}).toggle({ effect: "scale", direction: "vertical"});
     $( "#mouth" ).toggle({ effect: "scale", direction: "vertical" }).toggle({ effect: "scale", direction: "vertical" });
-    //alert("test");
-    //$("#lips").css("transform", "translate(-50%, -50%)");
-    //$("#mouth").css("transform", "translate(-50%, -50%)");
-    $(".hideQuote").fadeIn(4000);
+    $(".hideQuote").fadeIn(3000);
 }
 function looping() {
     $("#quoteMark").slideToggle(700, looping);
 }
 
+function getQuotes() {
+    return $.ajax({
+      headers: {
+        Accept: 'application/json'
+      },
+      url:
+        'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
+      success: function (jsonQuotes) {
+        if (typeof jsonQuotes === 'string') {
+          quotesData = JSON.parse(jsonQuotes);
+          console.log('quotesData');
+          console.log(quotesData);
+        }
+      }
+    });
+}
+
+function getRandomQuote() {
+    return quotesData.quotes[
+      Math.floor(Math.random() * quotesData.quotes.length)
+    ];
+  }
+
+function getQuote() {
+    let randomQuote = getRandomQuote();
+    currentQuote = randomQuote.quote + '"';
+    currentAuthor = "-" + randomQuote.author;
+}
+
 $(document).ready(function() {
+    getQuotes().then(() => {
+        getQuote();
+      });
     $("#quote").hide();
     looping();
     $("#randoButton").click(function() {
+        getQuote();
+        $(".dimmable").css("opacity", 1);
         $("#quote").hide();
         animateLips();
         setNewColor();
     });
     $("#quoteMark").click(function() {
-        $("#quote").fadeToggle(1000);
+        $("#quote").fadeIn(1000);
+        $('#tweet-quote-link').attr(
+            'href',
+            'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
+        );
+        $('#tumblr-quote-link').attr(
+            'href',
+            'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
+            encodeURIComponent(currentAuthor) +
+            '&content=' +
+            encodeURIComponent(currentQuote) +
+            '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
+        );
+        $('#quote-text').html(currentQuote);
+        $('#quote-author').html(currentAuthor);
+        $(".dimmable").css("opacity", 0.3);
+    });
+    $("#quote").click(function() {
+        $("#quote").fadeOut(1000);
+        $(".dimmable").css("opacity", 1);
     });
 });
